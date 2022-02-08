b21b7728b73e5c79a56644d24d6d80a9d60dbe7c
Derek <liutao3@cig.com.cn>
2022-02-08 22:05:51 +0800

    feat: save

--- src/BodyGame/body_detect.py
+++ src/BodyGame/body_detect.py
@@ -31,6 +31,7 @@ class BodyDetect:
                 h, w, c = img.shape
                 # mp_drawer.draw_landmarks(img, pose_results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
                 current_points = set()
+                current_points_pos = {}
                 for _id, lm in enumerate(pose_results.pose_landmarks.landmark):
                     # cv2.putText(img, str(_id), (int(lm.x * w) + 5, int(lm.y * h) + 5),
                     #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
@@ -40,10 +41,12 @@ class BodyDetect:
                         continue
                     cv2.circle(img, (int(lm.x * w), int(lm.y * h)), 5, (255, 0, 0), cv2.FILLED)
                     current_points.add(_id)
+                    current_points_pos[_id] = (lm.x, lm.y)
 
                 if current_points & self.right_hand_points == self.right_hand_points:
                     logger.critical('Detect Attack Action!')
                     attack_message = Message(action='ATTACK')
+                    # attack_message.mat_data = self.get_hand_area(self.right_hand_points, current_points_pos)
                     self.queue.put(attack_message)
                 if current_points & self.left_hand_points == self.left_hand_points:
                     defend_message = Message(action='DEFEND')
@@ -61,3 +64,19 @@ class BodyDetect:
         except Exception:
             logger.error('Failed to run, Error is:', exc_info=True)
             raise
+
+    @staticmethod
+    def get_hand_area(points, pos_dict, mat):
+        min_x = min_y = max_x = max_y = None
+        for _id in points:
+            x, y = pos_dict[_id]
+            if min_x is None or min_x > x:
+                min_x = x
+            if min_y is None or min_y > y:
+                min_y = y
+            if max_x is None or max_x < x:
+                max_x = x
+            if max_y is None or max_y < y:
+                max_y = y
+        area = mat[min_x-10:max_x+10, min_y-10:max_y+10]
+        return cv2.cvtColor(area, cv2.COLOR_RGB2BGR)
--- src/BodyGame/models.py
+++ src/BodyGame/models.py
@@ -3,9 +3,10 @@ import time
 
 class Message:
 
-    def __init__(self, action, timestamp=None):
+    def __init__(self, action, timestamp=None, mat_data=None):
         self.timestamp = timestamp or time.time()
         self.action = action
+        self.mat_data = mat_data
 
     def __str__(self):
         return f'Message[{self.action}/{self.timestamp}]'
